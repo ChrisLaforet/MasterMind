df2f1840b2df1eda0934a85a79fa7f4397bffd3f
Chris Laforet <laforet@chrislaforetsoftware.com>
2020-12-25 07:40:35 -0500

    Added image asset and trying to work out peg row

--- app/src/main/java/com/chrislaforetsoftware/mm/board/MastermindPegRow.kt
+++ app/src/main/java/com/chrislaforetsoftware/mm/board/MastermindPegRow.kt
@@ -1,10 +1,15 @@
 package com.chrislaforetsoftware.mm.board
 
 import android.content.Context
+import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.chrislaforetsoftware.mm.R
 
-class MastermindPegRow : View {
+class MastermindPegRow(context: Context, attrs: AttributeSet, defStyle: Int) : View(context, attrs, defStyle) {
+	init(attrs, defStyle)
+
 	constructor(context: Context) : super(context) {
 		init(null, 0)
 	}
@@ -13,13 +18,6 @@ class MastermindPegRow : View {
 		init(attrs, 0)
 	}
 
-	constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
-			context,
-			attrs,
-			defStyle
-	) {
-		init(attrs, defStyle)
-	}
 
 	private fun init(attrs: AttributeSet?, defStyle: Int) {
 		// Load attributes
@@ -28,5 +26,11 @@ class MastermindPegRow : View {
 //        )
 
 //        a.recycle()
+	}
+
+	fun onCreate(savedInstanceState: Bundle) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.mastermind_pegrow);
+
 	}
 }
