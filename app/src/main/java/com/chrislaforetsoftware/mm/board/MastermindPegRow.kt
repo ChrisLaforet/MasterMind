78d04e57efbb66e1ade73bd98049a5f95fd37d0f
Chris Laforet <laforet@chrislaforetsoftware.com>
2020-12-27 16:00:32 -0500

    Developing look of peg row

--- app/src/main/java/com/chrislaforetsoftware/mm/board/MastermindPegRow.kt
+++ app/src/main/java/com/chrislaforetsoftware/mm/board/MastermindPegRow.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.chrislaforetsoftware.mm.R
 
 
@@ -16,12 +17,35 @@ class MastermindPegRow(context: Context, attrs: AttributeSet?, defStyle: Int) : 
 
 	constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0) {}
 
+	private lateinit var rowNumber: TextView
+	private lateinit var peg0: MastermindPegHole
+	private lateinit var peg1: MastermindPegHole
+	private lateinit var peg2: MastermindPegHole
+	private lateinit var peg3: MastermindPegHole
+	private lateinit var peg4: MastermindPegHole
+	private lateinit var peg5: MastermindPegHole
+
 	init {
-//		val viewGroup = findViewById<View>(android.R.id.content) as ViewGroup
-
-//		val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-//		var view = inflater.inflate(R.layout.mastermind_pegrow, null)
-
 		inflate(this.context, R.layout.mastermind_pegrow, this)
+
+		rowNumber = findViewById<TextView>(R.id.row_number)
+		peg0 = findViewById<MastermindPegHole>(R.id.peg_0)
+		peg1 = findViewById<MastermindPegHole>(R.id.peg_1)
+		peg2 = findViewById<MastermindPegHole>(R.id.peg_2)
+		peg3 = findViewById<MastermindPegHole>(R.id.peg_3)
+		peg4 = findViewById<MastermindPegHole>(R.id.peg_4)
+		peg5 = findViewById<MastermindPegHole>(R.id.peg_5)
 	}
+
+	fun setNumber(number: Int) {
+		this.rowNumber.text = number.toString()
+	}
+
+	fun setWells(wellCount: Int) {
+		if (wellCount == 6) {
+			return
+		}
+
+	}
+
 }
--- app/src/main/java/com/chrislaforetsoftware/mm/board/PlayMastermindActivity.kt
+++ app/src/main/java/com/chrislaforetsoftware/mm/board/PlayMastermindActivity.kt
@@ -16,22 +16,29 @@ import com.chrislaforetsoftware.mm.R
 
 class PlayMastermindActivity : Activity() {
 
-    // ????
-//private lateinit var fullscreenContent: TextView
-
-
-
     private var totalWells: Int = 4
     private var totalColors: Int = 6
     private var allowDuplicateColors: Boolean = false
+
+    private lateinit var row0: MastermindPegRow
+    private lateinit var row1: MastermindPegRow;
+    private lateinit var row2: MastermindPegRow;
+    private lateinit var row3: MastermindPegRow;
+    private lateinit var row4: MastermindPegRow;
+    private lateinit var row5: MastermindPegRow;
+    private lateinit var row6: MastermindPegRow;
+    private lateinit var row7: MastermindPegRow;
+    private lateinit var row8: MastermindPegRow;
+    private lateinit var row9: MastermindPegRow;
 
     companion object {
         const val TOTAL_WELLS = "totalWells"
         const val TOTAL_COLORS = "totalColors"
         const val ALLOW_DUPLICATE_COLORS = "allowDuplicateColors"
+
+        const val MAX_WELLS = 6
     }
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -39,10 +46,43 @@ class PlayMastermindActivity : Activity() {
 
         setContentView(R.layout.activity_play_mastermind)
 
-
         totalWells = intent.getIntExtra(TOTAL_WELLS, 4)
         totalColors = intent.getIntExtra(TOTAL_COLORS, 6)
         allowDuplicateColors = intent.getBooleanExtra(ALLOW_DUPLICATE_COLORS, false)
+
+        row0 = findViewById<MastermindPegRow>(R.id.peg_row_0)
+        row0.setNumber(1)
+        row1 = findViewById<MastermindPegRow>(R.id.peg_row_1)
+        row1.setNumber(2)
+        row2 = findViewById<MastermindPegRow>(R.id.peg_row_2)
+        row2.setNumber(3)
+        row3 = findViewById<MastermindPegRow>(R.id.peg_row_3)
+        row3.setNumber(4)
+        row4 = findViewById<MastermindPegRow>(R.id.peg_row_4)
+        row4.setNumber(5)
+        row5 = findViewById<MastermindPegRow>(R.id.peg_row_5)
+        row5.setNumber(6)
+        row6 = findViewById<MastermindPegRow>(R.id.peg_row_6)
+        row6.setNumber(7)
+        row7 = findViewById<MastermindPegRow>(R.id.peg_row_7)
+        row7.setNumber(8)
+        row8 = findViewById<MastermindPegRow>(R.id.peg_row_8)
+        row8.setNumber(9)
+        row9 = findViewById<MastermindPegRow>(R.id.peg_row_9)
+        row9.setNumber(10)
+
+        if (totalWells != MAX_WELLS) {
+            row0.setWells(4)
+            row1.setWells(4)
+            row2.setWells(4)
+            row3.setWells(4)
+            row4.setWells(4)
+            row5.setWells(4)
+            row6.setWells(4)
+            row7.setWells(4)
+            row8.setWells(4)
+            row9.setWells(4)
+        }
 
     }
 
